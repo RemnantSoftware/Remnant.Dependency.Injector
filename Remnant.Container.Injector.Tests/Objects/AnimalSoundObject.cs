0f4b030ea89b55d0f94fc5273aeaf9ac6078ef5e
Neill Verreynne <39805506+RemnantSoftware@users.noreply.github.com>
2022-02-06 12:29:08 +0200

    Add files via upload

---
+++ Remnant.Container.Injector.Tests/Objects/AnimalSoundObject.cs
@@ -0,0 +1,25 @@
+﻿namespace Remnant.Container.Injector.Tests
+{
+	public class AnimalSoundObject
+	{
+		private IAnimal? _animal = RemContainer.Resolve<IAnimal>();
+
+		public AnimalSoundObject()
+		{
+		}
+
+		public IAnimal? Animal=> _animal;
+	}
+
+	public class AnimalSoundObject2
+	{
+		private IAnimal? _animal;
+
+		public AnimalSoundObject2()
+		{
+			_animal = this.Resolve<IAnimal>();
+		}
+
+		public IAnimal? Animal => _animal;
+	}
+}
