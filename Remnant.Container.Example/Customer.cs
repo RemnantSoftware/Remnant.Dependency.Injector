d7af7495fd56e7614d10da4d420f62738461b88e
Neill Verreynne <39805506+RemnantSoftware@users.noreply.github.com>
2022-02-06 12:29:42 +0200

    Add files via upload

---
+++ Remnant.Container.Example/Customer.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using Remnant.Container.Injector;
+
+namespace Remnant.ContainerInjector.Example
+{
+	public class Customer
+	{
+		private readonly ILog _logger;
+		private readonly EmailService _emailService;
+
+		public Customer()
+		{
+			_logger = this.Resolve<ILog>();
+			_logger.Warn("The logger has been resolved without constructor dependency injection.");
+			_logger.Warn("NB: This is just an illustration, NEVER use an email service from within a business domain class! :-)");
+
+			_emailService = this.Resolve<EmailService>();	
+		}
+
+		public string Name { get; set; }
+
+		public void SendMessage(string message)
+		{
+			_emailService.SendEmail(Name, message);
+		}
+	}
+}
---
+++ Remnant.Container.Example/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using log4net;
+using Remnant.Container.Injector;
+
+namespace Remnant.ContainerInjector.Example
+{
+	public class EmailService
+	{
+		private readonly ILog? _logger = RemContainer.Resolve<ILog>();
+
+
+		public void SendEmail(string recipient, string message)
+		{
+			_logger?.Debug($"Send email to {recipient}: {message}");
+		}
+	}
+}
